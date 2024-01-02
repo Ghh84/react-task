2e8f6c654698acd3a798ca201a4bd63cd018bdd5
Ghh84 <ghiwethh@gmail.com>
2024-01-02 11:46:55 +0200

    adding stat and function

--- counter-app/src/components/counterComponent.jsx
+++ counter-app/src/components/counterComponent.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
-    render() { 
-        return (<h1>Hello React</h1>);
-    }
+  state = {
+    count: 0,
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <span>{this.formateCount()}</span>
+        <button>Increament</button>
+      </React.Fragment>
+    )
+  }
+  formateCount() {
+    const { count } = this.state
+    return count == 0 ? 'zero' : count
+  }
 }
- 
-export default Counter;
+
+export default Counter
