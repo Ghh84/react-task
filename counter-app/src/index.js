64056900cc90a89461344b2a11fb2dbe8b3598c7
Ghh84 <ghiwethh@gmail.com>
2024-01-02 11:10:50 +0200

    adding counter class to index

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counterComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>
 );
 
