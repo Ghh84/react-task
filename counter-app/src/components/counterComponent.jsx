3954c6dd7f43867aa415f5c8b82f198bf1180f46
Ghh84 <ghiwethh@gmail.com>
2024-01-03 12:00:46 +0200

    Adding inline style.

--- counter-app/src/components/counterComponent.jsx
+++ counter-app/src/components/counterComponent.jsx
@@ -7,8 +7,13 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <span>{this.formateCount()}</span>
-        <button>Increament</button>
+        <span
+          style={{ fontSize: 20, alignItems: 'center' }}
+          className="badge badge-primary m-2"
+        >
+          {this.formateCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increament</button>
       </React.Fragment>
     )
   }
