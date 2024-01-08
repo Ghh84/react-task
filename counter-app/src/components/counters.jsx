7c3dbab8a76ddb972bb0bc0cf51a52359d2ff583
Ghh84 <ghiwethh@gmail.com>
2024-01-08 17:41:31 +0200

    Adding delete button.

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Counter from './counterComponent'
+import Counter from './counter'
 class Counters extends Component {
   state = {
     counters: [
@@ -9,13 +9,41 @@ class Counters extends Component {
       { id: 4, value: 0 },
     ],
   }
+  handleIncreament = (counter) => {
+    const counters = [...this.state.counters]
+    const index = counters.indexOf(counter)
+    counter[index] = { ...counter }
+    counters[index].value++
+
+    this.setState({ counters })
+  }
+  handleDelete = (id) => {
+    const counters = this.state.counters.filter((m) => m.id !== id)
+    this.setState({ counters })
+  }
+  handleRest = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0
+      return c
+    })
+    this.setState({ counters })
+  }
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleRest}
+          className="btn  btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
             value={counter.value}
+            onIncreament={() => this.handleIncreament(counter)}
+            onDelete={() => this.handleDelete(counter.id)}
+            onReset={() => this.handleRest()}
             id={counter.id}
             selected
           />
